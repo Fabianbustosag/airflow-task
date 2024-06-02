d8053e29a36c444fcfefda75deffa03ca9c4977d
nottfenrir <fabianbustosag@gmail.com>
2024-06-02 13:40:30 -0400

    final supogo

--- dags/README.md
+++ dags/README.md
@@ -1,4 +1,5 @@
-![Alt text](image_airflow.png)
+# Airflow Task Paralelismo DAGS
 
+![Alt text](image_airflow.png)
 
 - Se cambio el archivo query_sql y se ingresaron los datos de la base de datos en vez de .ini a un diccionario de python
--- dags/test1.py
+++ dags/test1.py
@@ -16,34 +16,8 @@ dag_args={
     "retry_delay": timedelta(minutes=5),
 }
 
-# Crear las DAGs
-# dag_ids = ['dag1', 'dag2', 'dag3']
-# inicio_values = [0, 5, 10]  # Valores de inicio diferentes para cada DAG
-
-# for dag_id, inicio in zip(dag_ids, inicio_values):
-#     with DAG(
-#         dag_id,
-#         default_args=dag_args,
-#         description=f'Parallel DAG {dag_id}',
-#         schedule_interval=timedelta(days=1),
-#         start_date=datetime(2023, 1, 1),
-#         catchup=False,
-#     ) as dag:
-        
-#         # Definir el PythonOperator
-#         run_funcion_paralela = PythonOperator(
-#             task_id='run_funcion_paralela',
-#             python_callable=airflow_task,
-#             op_kwargs={'inicio': inicio, 'leer': 5},  # Ajusta los parámetros según sea necesario
-#         )
-
-#         globals()[dag_id] = dag
-
-
-#-------------------------
-
-dag_test = DAG(
-    "dag_test",
+dag = DAG(
+    "dag",
     description="A simple tutorial DAG",
     default_args=dag_args,
     schedule=timedelta(days=1),
@@ -58,27 +32,27 @@ def test():
 t0 = PythonOperator(
     task_id='inicio_funcion_paralela',
     python_callable=test,
-    dag=dag_test
+    dag=dag
 )
 
 t1 = PythonOperator(
     task_id='funcion_paralela1',
     python_callable=task_etl,
-    dag=dag_test,
+    dag=dag,
     op_kwargs={'start_offset': 0},
 )
 
 t2 = PythonOperator(
     task_id='funcion_paralela2',
     python_callable=task_etl,
-    dag=dag_test,
+    dag=dag,
     op_kwargs={'start_offset': 5},
 )
 
 t3 = PythonOperator(
     task_id='funcion_paralela3',
     python_callable=task_etl,
-    dag=dag_test,
+    dag=dag,
     op_kwargs={'start_offset': 10},
 )
 
